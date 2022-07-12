6559e591de33f699ce71151ac06515132f99f44a
Mislav Matijević <mislav.mim@gmail.com>
2022-07-12 10:22:50 +0200

    Renamed login activity. Original name was "main activity".

--- app/src/main/java/org/foi/mmatijevi/air/virtualmarketplacetest/LoginActivity.kt
+++ app/src/main/java/org/foi/mmatijevi/air/virtualmarketplacetest/LoginActivity.kt
@@ -3,7 +3,7 @@ package org.foi.mmatijevi.air.virtualmarketplacetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
