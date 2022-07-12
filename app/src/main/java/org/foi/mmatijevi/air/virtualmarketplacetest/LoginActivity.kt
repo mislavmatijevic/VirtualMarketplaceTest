5509dbfc30184cd35141a2199de4043075f2899f
Mislav Matijević <mislav.mim@gmail.com>
2022-07-12 15:00:43 +0200

    Added Register Activity. It will host fragments which take care of registering users.

--- app/src/main/java/org/foi/mmatijevi/air/virtualmarketplacetest/LoginActivity.kt
+++ app/src/main/java/org/foi/mmatijevi/air/virtualmarketplacetest/LoginActivity.kt
@@ -1,11 +1,18 @@
 package org.foi.mmatijevi.air.virtualmarketplacetest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val registerButton = findViewById<Button>(R.id.btnRegister)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/org/foi/mmatijevi/air/virtualmarketplacetest/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package org.foi.mmatijevi.air.virtualmarketplacetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
